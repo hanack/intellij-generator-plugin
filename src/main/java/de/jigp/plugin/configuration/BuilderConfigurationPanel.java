3711654556ce4065301176d276cb562ceb9241c4
jens.hanack <jens@imac.local>
2009-10-04 16:36:07 +0200

    refactorings

--- src/main/java/de/jigp/plugin/configuration/BuilderConfigurationPanel.java
+++ src/main/java/de/jigp/plugin/configuration/BuilderConfigurationPanel.java
@@ -17,6 +17,41 @@ public class BuilderConfigurationPanel extends JPanel {
 
     private void initPanel() {
         addBorder();
+        initLayout();
+
+        setXY(0, 0);
+        this.add(new JLabel("Annotation (full qualified)"), constraints);
+
+        setXY(1, 0);
+        setWeightX(1.0);
+        this.add(annotation, constraints);
+
+        setXY(0, 1);
+        setWeightX(0);
+        this.add(new JLabel("Static assertion method (full qualified)"), constraints);
+
+        setXY(1, 1);
+        setWeightX(1.0);
+        setWeightY(1.0);
+        constraints.fill = GridBagConstraints.BOTH;
+        this.add(assertionExpression, constraints);
+
+    }
+
+    private void setWeightY(double y) {
+        constraints.weighty = y;
+    }
+
+    private void setWeightX(double x) {
+        constraints.weightx = x;
+    }
+
+    private void setXY(int x, int y) {
+        constraints.gridx = x;
+        constraints.gridy = y;
+    }
+
+    private void initLayout() {
         annotation = new JTextField();
         assertionExpression = new JTextField();
 
@@ -28,28 +63,6 @@ public class BuilderConfigurationPanel extends JPanel {
         constraints.insets = new Insets(0, 0, 0, 0);
         constraints.gridwidth = 1;
         constraints.gridheight = 1;
-
-
-        constraints.gridx = 0;
-        constraints.gridy = 0;
-        this.add(new JLabel("Annotation (full qualified)"), constraints);
-        constraints.gridx = 1;
-        constraints.gridy = 0;
-        constraints.weightx = 1.0;
-        this.add(annotation, constraints);
-
-
-        constraints.gridx = 0;
-        constraints.gridy = 1;
-        constraints.weightx = 0;
-        this.add(new JLabel("Static assertion method (full qualified)"), constraints);
-        constraints.gridx = 1;
-        constraints.gridy = 1;
-        constraints.weightx = 1.0;
-        constraints.weighty = 1.0;
-        constraints.fill = GridBagConstraints.BOTH;
-        this.add(assertionExpression, constraints);
-
     }
 
     private void addBorder() {
