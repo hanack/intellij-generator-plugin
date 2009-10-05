6692387473d9761eff3ae4610f78c77cf390b68b
jens.hanack <jens@imac.local>
2009-10-05 08:14:40 +0200

    no add/remove method for HashMaps configured.

--- src/main/java/de/jigp/plugin/configuration/TypeToTextFactory.java
+++ src/main/java/de/jigp/plugin/configuration/TypeToTextFactory.java
@@ -9,10 +9,11 @@ public class TypeToTextFactory {
                 .put("java.util.Collection", "java.util.Collections." + GENERICS_PLACEHOLDER + "emptySet()", true)
                 .put("java.util.Set", "java.util.Collections." + GENERICS_PLACEHOLDER + "emptySet()", true)
                 .put("java.util.List", "java.util.Collections." + GENERICS_PLACEHOLDER + "emptyList()", true)
-                .put("java.util.HashSet", "new java.util.HashSet" + GENERICS_PLACEHOLDER + "()", true)
                 .put("java.util.ArrayList", "new java.util.ArrayList" + GENERICS_PLACEHOLDER + "()", true)
                 .put("java.util.SortedSet", "new java.util.TreeSet" + GENERICS_PLACEHOLDER + "()", true)
-                .put("java.util.HashMap", "new java.util.HashMap" + GENERICS_PLACEHOLDER + "()", true)
-                .put("java.util.LinkedHashSet", "new java.util.LinkedHashSet" + GENERICS_PLACEHOLDER + "()", true);
+                .put("java.util.HashSet", "new java.util.HashSet" + GENERICS_PLACEHOLDER + "()", true)
+                .put("java.util.LinkedHashSet", "new java.util.LinkedHashSet" + GENERICS_PLACEHOLDER + "()", true)
+                .put("java.util.HashMap", "new java.util.HashMap" + GENERICS_PLACEHOLDER + "()", false)
+                ;
     }
 }
